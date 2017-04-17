0bd7b1e601f2dfbc59a59688258115da355b3090
buji <buji@mogujie.com>
2017-04-17 19:10:17 +0800

    feat: up top rank

--- pages/randomeat/index.js
+++ pages/randomeat/index.js
@@ -2,14 +2,15 @@ Page({
     data: {
         motto: '准备好了吗？',
         clicked: false,
-        num: 1,
-        kinds: [
+        kinds: [ // default
             '梅干菜骚饼',
             '油泼面',
             '龙虾盖浇饭',
             '豆腐年糕',
             '蛋炒饭'
-        ]
+        ],
+        rank: [],
+        top: []
     },
     onLoad:function() {
         wx.setNavigationBarTitle({
@@ -31,25 +32,65 @@ Page({
           }
         })
     },
+    rankExist(rank, kind) {
+        for (
+            let i = 0, len = rank.length;
+            i < len;
+            i++
+        ) {
+            if (rank[i].name === kind) {
+                return true
+            }
+        }
+        return false
+    },
+    updateRank(rank, kind) {
+        if (this.rankExist(rank, kind)) {
+            for (
+                let i = 0, len = rank.length;
+                i < len;
+                i++
+            ) {
+                if (rank[i].name === kind) {
+                    rank[i].num++
+                    break;
+                }
+            }
+            rank = rank.sort((a, b) => a.num < b.num)
+        } else {
+            rank.push({
+                name: kind,
+                num: 1
+            })
+        }
+        return rank
+    },
+    searchRank(rank, kind) {
+        for (
+            let i = 0, len = rank.length;
+            i < len;
+            i++
+        ) {
+            if (rank[i].name === kind) {
+                return rank[i].num
+            }
+        }
+        return 1
+    },
     go: function() {
         this.setData({
             clicked: true
         })
 
         let kind = this.data.kinds[Math.random() * this.data.kinds.length >>> 0]
-        let num = this.data.num
-        const code = ' combo x'
 
-        if (this.data.motto.split(code)[0] === kind) {
-            num++
-            kind += code + num
-        } else {
-            num = 1
-        }
-
+        let rank = this.data.rank
+        rank = this.updateRank(rank, kind)
+        
+        let num = this.searchRank(rank, kind)
         this.setData({
-            motto: kind,
-            num: num
+            motto: kind + (num > 1 ? ' combo x' + num : ''),
+            top: rank.slice(0, 3)
         })
     }
 })
